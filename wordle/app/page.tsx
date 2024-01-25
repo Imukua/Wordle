6c01796328ad3b0f3e739301c5e5d7cdeb51c629
Imukua <ianmukuaa@gmail.com>
2024-01-25 20:38:29 +0300

    implemented grid

--- wordle/app/page.tsx
+++ wordle/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
+import GridRow from "./GridRow";
 
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <GridRow />
+      <GridRow />
+      <GridRow />
+      <GridRow />
+      <GridRow />
+      <GridRow />
+    </div>
   );
 }
